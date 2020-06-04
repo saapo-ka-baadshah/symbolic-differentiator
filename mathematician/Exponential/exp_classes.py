86033ab445de310b5009c09a18293ca8f662a92a
BhaskarTelecom <bhaskar4telecom@gmail.com>
2020-06-04 14:43:56 +0530

    added inverse, exp classes

--- mathematician/Exponential/exp_classes.py
+++ mathematician/Exponential/exp_classes.py
@@ -1,8 +1,60 @@
 """
 Defines all Exponential Classes
 
-Ax(		radix, exponent 	):				defines an radic^(exponent) object (for natural log)
+Ax(		radix, exponent 	):				defines a radic^(exponent) object (for common log)
 
-Ex(		exponent 		):		
+Ex(		exponent 		):		            defines exp^(exponent) object
 
 """
+
+
+class Ex():
+    """
+    >>> exp_obj = Ex(exponent='x')
+    >>> str(exp_obj)
+    'e^{x}'
+
+    >>> exp_obj.derivative()
+    'e^{x}'
+
+
+
+    """
+
+    def __init__(self, exponent):
+        self.exponent = exponent
+
+    def __str__(self):
+        out = "e^{%s}" % (str(self.exponent))
+        return out
+
+    def derivative(self):
+        out = "e^{%s}" % (str(self.exponent))
+        return out
+
+
+class Ax():
+    r"""
+    >>> exp_obj = Ax(exponent='x',radix='a')
+    >>> str(exp_obj)
+    'a^{x}'
+
+    >>> exp_obj.derivative()
+    'a^{x}*\\log(a)'
+
+
+
+    """
+
+    def __init__(self, exponent, radix):
+        self.exponent = exponent
+        self.radix = radix
+
+    def __str__(self):
+        out = "%s^{%s}" % (str(self.radix), str(self.exponent))
+        return out
+
+    def derivative(self):
+        out = r"%s^{%s}*\log(%s)" % (str(self.radix),
+                                     str(self.exponent), str(self.radix))
+        return out
--- mathematician/Inverse/inverse_class.py
+++ mathematician/Inverse/inverse_class.py
@@ -1,21 +1,21 @@
 """
-sin     ( val )   :         Defines the regular expressions for trigonometric functions namely sin(),cos(),tan(),cosec(),sec(),cot()
-cos     ( val )   :
-tan     ( val )   :
-sec     ( val )   :
-cosec   ( val )   :
-cot     ( val )   :
+sin_inv     ( val )   :         Defines the regular expressions for inverse trigonometric functions namely asin(),acos(),atan(),acosec(),asec(),acot()
+cos_inv     ( val )   :
+tan_inv     ( val )   :
+secIinv     ( val )   :
+cosec_inv   ( val )   :
+cot_inv     ( val )   :
 """
 
 
 class sin_inv():
-    """
+    r"""
     >>> sin_obj=sin_inv(val='x')
     >>> str(sin_obj)
     'sin^{-1}(x)'
 
     >>> sin_obj.derivative()
-    '1/\\\sqrt{1-x^{2}}'
+    '\\frac{1}{\\sqrt{1-x^{2}}}'
 
     """
 
@@ -27,5 +27,120 @@ class sin_inv():
         return out
 
     def derivative(self):
-        out = '1/\\' + 'sqrt{1-%s^{2}}' % (str(self.val))
+        out = r'\frac{1}{\sqrt{1-%s^{2}}}' % (str(self.val))
+        return out
+
+
+class cos_inv():
+    r"""
+    >>> cos_obj=cos_inv(val='x')
+    >>> str(cos_obj)
+    'cos^{-1}(x)'
+
+    >>> cos_obj.derivative()
+    '\\frac{1}{\\sqrt{x^{2}-1}}'
+
+    """
+
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        out = "cos^{-1}(%s)" % (str(self.val))
+        return out
+
+    def derivative(self):
+        out = r'\frac{1}{\sqrt{%s^{2}-1}}' % (str(self.val))
+        return out
+
+
+class tan_inv():
+    r"""
+    >>> tan_obj=tan_inv(val='x')
+    >>> str(tan_obj)
+    'tan^{-1}(x)'
+
+    >>> tan_obj.derivative()
+    '\\frac{1}{1+x^{2}}'
+
+    """
+
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        out = "tan^{-1}(%s)" % (str(self.val))
+        return out
+
+    def derivative(self):
+        out = r'\frac{1}{1+%s^{2}}' % (str(self.val))
+        return out
+
+
+class cot_inv():
+    r"""
+    >>> cot_obj=cot_inv(val='x')
+    >>> str(cot_obj)
+    'cot^{-1}(x)'
+
+    >>> cot_obj.derivative()
+    '\\frac{-1}{1+x^{2}}'
+
+    """
+
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        out = "cot^{-1}(%s)" % (str(self.val))
+        return out
+
+    def derivative(self):
+        out = r'\frac{-1}{1+%s^{2}}' % (str(self.val))
+        return out
+
+
+class sec_inv():
+    r"""
+    >>> sec_obj=sec_inv(val='x')
+    >>> str(sec_obj)
+    'sec^{-1}(x)'
+
+    >>> sec_obj.derivative()
+    '\\frac{1}{|x|*\\sqrt{x^{2}-1}}'
+
+    """
+
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        out = "sec^{-1}(%s)" % (str(self.val))
+        return out
+
+    def derivative(self):
+        out = r'\frac{1}{|x|*\sqrt{%s^{2}-1}}' % (str(self.val))
+        return out
+
+
+class cosec_inv():
+    r"""
+    >>> cosec_obj=cosec_inv(val='x')
+    >>> str(cosec_obj)
+    'cosec^{-1}(x)'
+
+    >>> cosec_obj.derivative()
+    '\\frac{-1}{|x|*\\sqrt{x^{2}-1}}'
+
+    """
+
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        out = "cosec^{-1}(%s)" % (str(self.val))
+        return out
+
+    def derivative(self):
+        out = r'\frac{-1}{|x|*\sqrt{%s^{2}-1}}' % (str(self.val))
         return out
